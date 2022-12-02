278f94e4cc822d50c9d51288245ecec3491bf842
kyle.l.matuszczak <usacys@local>
2022-12-02 07:22:09 -0500

    Refactored to have a generator

--- day01/part2.py
+++ day01/part2.py
@@ -3,18 +3,21 @@ from sys import argv
 from traceback import print_exc
 
 
-def main():
-    total = 0
-    everyone = []
-    with open("input.txt", "r") as file:
-        for line in file:
-            try:
-                total += int(line)
-            except ValueError:
-                everyone.append(total)
-                total = 0
-    print(sum(sorted(everyone, reverse=True)[:3]))
+def get_elf_cals(raw_data):
+    elf = []
+    for entry in raw_data:
+        if entry == '\n':
+            yield elf
+            elf = []
+        else:
+            elf.append(int(entry))
 
+def main():
+    with open("input.txt", "r") as file:
+        elves = [sum(pack) for pack in get_elf_cals(file.readlines())]
+        elves = sorted(elves, reverse=True)
+
+    print(f"sum({elves[:3]}) = {sum(elves[:3])}")
 
 
 if __name__ == '__main__':
