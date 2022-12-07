0d217aebdd4ea812424ee90e3233761d2c3b3087
kyle.l.matuszczak <usacys@local>
2022-12-07 09:29:20 -0500

    Explained why filereading was different

--- day03/part2.py
+++ day03/part2.py
@@ -6,18 +6,23 @@ def main():
     total = 0
     idx = 0
 
+    # read entire file at once to allow for handling multi-line task
     data = file.readlines()
     length = len(data)
 
     while idx < length:
+        # establish set for each 3 lines
         a = set(data[idx].strip())
         b = set(data[idx + 1].strip())
         c = set(data[idx + 2].strip())
 
+        # find intersecting character of all 3 lines
         result = list(a.intersection(b.intersection(c)))[0]
 
+        # convert to 1's index ascii
         total += ord(result) % 32
         if not result.islower():
+            # add 26 for uppercase
             total += 26
 
         idx += 3
