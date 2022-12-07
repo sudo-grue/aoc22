faf5a7ce104b69f0c6a290d0d72233923d92da7e
kyle.l.matuszczak <usacys@local>
2022-12-07 09:26:57 -0500

    Added comments explaining set logic used

--- day03/part1.py
+++ day03/part1.py
@@ -7,13 +7,18 @@ def main():
 
     for line in file:
         length = len(line.strip())
+        # create set for first half
         a = set(line[:length//2])
+        # create set for second half
         b = set(line[length//2:])
 
+        # find intersection between sets (commonality)
         result = list(a.intersection(b))[0]
 
+        # convert character to 1's index of ascii
         total += ord(result) % 32
         if not result.islower():
+            # add 26 for uppercase
             total += 26
 
     file.close()
