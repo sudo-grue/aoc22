a2e9f461e6bea939530003fe8ce2565bf762dc13
Kyle Matuszczak <kyle.l.matusczak.mil@army.mil>
2022-12-09 17:45:58 -0500

    Written at work, had to rewrite from home

---
+++ day09/combo.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+class Rope:
+    db = {"U": (1, 0), "D": (-1, 0), "L": (0, -1), "R": (0, 1),
+          2: 1, -2: -1, 1: 1, -1: -1, 0: 0}
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.been = set()
+
+    def move(self, act):
+        x, y = self.db[act]
+        self.x += x
+        self.y += y
+
+    def follow(self, other):
+        x = other.x - self.x
+        y = other.y - self.y
+
+        if 2 == abs(x) or 2 == abs(y):
+            self.x += self.db[x]
+            self.y += self.db[y]
+
+        self.been.add((self.x, self.y))
+
+
+def main():
+    head = Rope()
+    part1 = Rope()
+    two = Rope()
+    three = Rope()
+    four = Rope()
+    five = Rope()
+    six = Rope()
+    seven = Rope()
+    eight = Rope()
+    part2 = Rope()
+
+    with open("input.txt", "r") as file:
+        for line in file:
+            act, times = line.split()
+            for _ in range(int(times)):
+                head.move(act)
+                part1.follow(head)
+                two.follow(part1)
+                three.follow(two)
+                four.follow(three)
+                five.follow(four)
+                six.follow(five)
+                seven.follow(six)
+                eight.follow(seven)
+                part2.follow(eight)
+
+    print(f"{len(part1.been) = }")
+    print(f"{len(part2.been) = }")
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except (SystemExit, KeyboardInterrupt, GeneratorExit, Exception):
+        from traceback import print_exc
+        print_exc()
+
---
+++ day09/part1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+class Rope:
+    db = {"U": (1, 0), "D": (-1, 0), "L": (0, -1), "R": (0, 1),
+          2: 1, -2: -1, 1: 1, -1: -1, 0: 0}
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.been = set()
+
+    def move(self, act):
+        x, y = self.db[act]
+        self.x += x
+        self.y += y
+
+    def follow(self, other):
+        x = other.x - self.x
+        y = other.y - self.y
+
+        if 2 == abs(x) or 2 == abs(y):
+            self.x += self.db[x]
+            self.y += self.db[y]
+
+        self.been.add((self.x, self.y))
+
+
+def main():
+    head = Rope()
+    tail = Rope()
+
+    with open("input.txt", "r") as file:
+        for line in file:
+            act, times = line.split()
+            for _ in range(int(times)):
+                head.move(act)
+                tail.follow(head)
+
+    print(len(tail.been))
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except (SystemExit, KeyboardInterrupt, GeneratorExit, Exception):
+        from traceback import print_exc
+        print_exc()
+
