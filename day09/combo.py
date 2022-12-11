ad8f8e55221d9560c98a6ae715f0bfdb7c371fc1
sudo-grue <kyle.l.matuszczak@gmail.com>
2022-12-11 18:51:37 -0500

    Moved docstring to module

--- day09/combo.py
+++ day09/combo.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+"""
+The head of a rope receives directions of movement, when head is more
+than 1 square away (including diagonal) from rest of rope, the rest of
+the rope must follow
+
+Part 1: Tail is the next segment of rope behind head and can never be more
+than +/- 2 squares in one direction and +/- 1 squares in the other.
+
+Part 2: Tail is the 10th segment and each segment must follow the previous
+one. This creates the possibility of up to +/- 2 in both/either cardinal
+directions. 
+
+Directions appear in format 2 part format where the
+first portion is U, D, L, R and second portion is number of
+squares to move in that direction.
+"""
 
 class Rope:
     db = {"U": (1, 0), "D": (-1, 0), "L": (0, -1), "R": (0, 1),
@@ -26,22 +42,6 @@ class Rope:
 
 
 def main():
-    """
-    The head of a rope receives directions of movement, when head is more
-    than 1 square away (including diagonal) from rest of rope, the rest of
-    the rope must follow
-
-    Part 1: Tail is the next segment of rope behind head and can never be more
-    than +/- 2 squares in one direction and +/- 1 squares in the other.
-
-    Part 2: Tail is the 10th segment and each segment must follow the previous
-    one. This creates the possibility of up to +/- 2 in both/either cardinal
-    directions. 
-
-    Directions appear in format 2 part format where the
-    first portion is U, D, L, R and second portion is number of
-    squares to move in that direction.
-    """
     head = Rope()
     part1 = Rope()
     two = Rope()
